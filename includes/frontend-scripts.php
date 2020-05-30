12e6b0c5fe1872e4ecde34a51efe6f027e25ffa1
Matt Cromwell <reachme@mattcromwell.com>
2020-05-29 22:27:48 -0700

    Output JSON-LD and start pulling dynamic info on the Donation Confirmation pixel.

--- includes/frontend-scripts.php
+++ includes/frontend-scripts.php
@@ -15,6 +15,7 @@ function my_footer_scripts(){
             jQuery(window).load(setTimeout(givewpTriggerPurchaseEvent, 5500));
 
             function givewpTriggerPurchaseEvent(){
+
                 var donationReceipt = jQuery('#give_donation_receipt').length;
 
                 var successPage = jQuery('p.give_notice.give_success').length;
@@ -25,13 +26,45 @@ function my_footer_scripts(){
 
                 if(successPage) {
                     console.log('This is a Donation Confirmation Page');
-                    var getPrice = jQuery('#totaldonation').text();
-                    getPrice = getPrice.split('$');
-                    getPrice = Number(getPrice[1]);
-                    fbq('track', 'Purchase', {currency: give_global_vars.currency[0] , value: getPrice});
+                    var data =  jQuery('#givewppt4fb').html();
+                    var jsonld = JSON.parse(data);
+                    fbq('track', 'Purchase', {currency: jsonld.priceCurrency, value: jsonld.price});
                 }
             }
         }
     </script>
   <?php
+}
+
+add_action('give_payment_receipt_after_table', 'givewppt4fb_jsonld_output');
+
+function givewppt4fb_jsonld_output() {
+
+    global $give_receipt_args, $donation;
+    $meta = get_post_meta($donation->ID);
+    $amount = number_format($meta['_give_payment_total'][0], 2, '.',',');
+    $currency = $meta['_give_payment_currency'][0];
+    $firstname = $meta['_give_donor_billing_first_name'][0];
+    $lastname = $meta['_give_donor_billing_last_name'][0];
+
+    var_dump($meta);
+
+    ?>
+        <script type="application/ld+json" id="givewppt4fb">
+        {
+            "@context": "http://schema.org",
+            "@type": "DonateAction",
+            "agent": {
+                "@type": "Person", // Check for company/individual
+                "name": "<?php echo $firstname . ' ' . $lastname; ?>"
+            },
+            "price": "<?php echo $amount; ?>",
+            "priceCurrency": "<?php echo $currency; ?>",
+            "recipient": {
+                "@type": "Person", // Create setting to switch person/organization 
+                "name": "Steve" // Pull from Settings
+            }
+        }
+        </script>
+    <?php 
 }
