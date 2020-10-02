04ad0147e29b4653cb9a99fd045d6b3d3c744791
Matt Cromwell <reachme@mattcromwell.com>
2020-10-01 17:04:17 -0700

    Legacy forms are working. Sequoia is not. But the basics are there for future testing.

--- assets/js/givewppt4fb-frontend.js
+++ assets/js/givewppt4fb-frontend.js
@@ -1,11 +1,13 @@
 if (typeof window.fbq !== "undefined") { 
-    console.log('The FB Pixel was detected');
+    console.log('An FB Pixel was detected! The GiveWP Donation Pixel can now fire!');
     const form = document.querySelector('form[id*="give-form"]');
     const amount = document.querySelector('.give-final-total-amount');
-    var donateclick = document.getElementById("give-purchase-button")
+    var donateclick = document.getElementById("give-purchase-button");
     
     donateclick.onclick(
-    fbq('track', 'Purchase', {currency: form.dataset.currency_code, value: amount})
+        fbq('track', 'Purchase', {currency: form.dataset.currency_code, value: amount}),
+
+        console.log('The GiveWP Pixel fired with a value of:' + form.dataset.currency_code + ' ' + amount)
     );
 }
 
--- includes/frontend-scripts.php
+++ includes/frontend-scripts.php
@@ -1,39 +1,63 @@
 <?php 
 
-add_action( 'wp_footer', 'my_footer_scripts' );
+add_action( 'wp_footer', 'givewppt4fb_footer_scripts' );
+add_action( 'give_new_receipt', 'givewppt4fb_sequoia_scripts');
 
-function my_footer_scripts(){
+function givewppt4fb_footer_scripts(){
   ?>
     <script type="text/javascript">
         if ( undefined !== window.fbq ) {
             console.log('The FB Pixel was detected');
-
-            jQuery("input#give-purchase-button").click(function($){
-                fbq('track', 'Donate')
-            } );
         
-            jQuery(window).load(setTimeout(givewpTriggerPurchaseEvent, 5500));
+            jQuery(window).load(setTimeout(givewpTriggerPurchaseEvent, 1000));
 
             function givewpTriggerPurchaseEvent(){
 
                 var donationReceipt = jQuery('#give_donation_receipt').length;
-
                 var successPage = jQuery('p.give_notice.give_success').length;
-
                 var verifyTotalDonation = jQuery('#totaldonation').length;
 
-                console.log(donationReceipt, successPage, verifyTotalDonation)
+                console.log(donationReceipt, successPage, verifyTotalDonation);
 
                 if(successPage) {
                     console.log('This is a Donation Confirmation Page');
                     var data =  jQuery('#givewppt4fb').html();
                     var jsonld = JSON.parse(data);
+
+                    fbq('track', 'Donate')
+
                     fbq('track', 'Purchase', {currency: jsonld.priceCurrency, value: jsonld.price});
+
+                    console.log(jsonld);
                 }
             }
         }
     </script>
   <?php
+}
+
+function givewppt4fb_sequoia_scripts() {
+    ?>
+        <script type="text/javascript" id="givewppt4fb">
+            var console = {
+                    panel: jQuery(parent.document.body).append('<div>'),
+                    log: function(m){
+                    this.panel.prepend('<div>'+m+'</div>');
+                }       
+            };
+            var multiStepDonateButton = jQuery('.give-viewing-form-in-iframe .give-submit').length;
+            var isMultistep = false;
+
+            if (jQuery('body.give-form-templates')[0]) {
+                var isMultistep = true;
+            }
+
+            console.log('This is the Multi-Step Receipt page!');
+
+            console.log('Is this is a multi-step form?' + isMultistep);
+
+        </script>
+    <?php 
 }
 
 add_action('give_payment_receipt_after_table', 'givewppt4fb_jsonld_output');
@@ -47,24 +71,20 @@ function givewppt4fb_jsonld_output() {
     $firstname = $meta['_give_donor_billing_first_name'][0];
     $lastname = $meta['_give_donor_billing_last_name'][0];
 
-    var_dump($meta);
+    //var_dump($meta);
 
     ?>
         <script type="application/ld+json" id="givewppt4fb">
-        {
-            "@context": "http://schema.org",
-            "@type": "DonateAction",
-            "agent": {
-                "@type": "Person", // Check for company/individual
-                "name": "<?php echo $firstname . ' ' . $lastname; ?>"
-            },
-            "price": "<?php echo $amount; ?>",
-            "priceCurrency": "<?php echo $currency; ?>",
-            "recipient": {
-                "@type": "Person", // Create setting to switch person/organization 
-                "name": "Steve" // Pull from Settings
+            {
+                "@context": "http://schema.org",
+                "@type": "DonateAction",
+                "agent": {
+                    "@type": "Person",
+                    "name": "<?php echo $firstname . ' ' . $lastname; ?>"
+                },
+                "price": "<?php echo $amount; ?>",
+                "priceCurrency": "<?php echo $currency; ?>"
             }
-        }
         </script>
     <?php 
 }
