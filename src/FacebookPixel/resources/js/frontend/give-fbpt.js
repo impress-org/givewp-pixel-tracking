28348874db36133c8fead15014e07b199560ca59
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-10-16 18:28:18 -0400

    fix: remove console log

--- src/FacebookPixel/resources/js/frontend/give-fbpt.js
+++ src/FacebookPixel/resources/js/frontend/give-fbpt.js
@@ -2,7 +2,6 @@ const { fbq } = window.parent ? window.parent : window;
 const { giveFBPT } = window;
 
 if (typeof fbq !== "undefined") { 
-    console.log('fbq event!!', giveFBPT);
     fbq('track', 'Donate' );
     fbq('track', 'Purchase', {currency: giveFBPT.currency, value: giveFBPT.amount});
 }
