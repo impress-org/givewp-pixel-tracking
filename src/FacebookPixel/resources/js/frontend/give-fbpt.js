f87ae5c6b75fc8d0b0cb213a63f0f3afe470816b
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-10-16 18:22:50 -0400

    fix: correct syntax error

--- src/FacebookPixel/resources/js/frontend/give-fbpt.js
+++ src/FacebookPixel/resources/js/frontend/give-fbpt.js
@@ -2,6 +2,7 @@ const { fbq } = window.parent ? window.parent : window;
 const { giveFBPT } = window;
 
 if (typeof fbq !== "undefined") { 
+    console.log('fbq event!!', giveFBPT);
     fbq('track', 'Donate' );
-    fbq('track', 'Purchase', {currency: giveFBPT.currency, value: giveFBPT.amount}),
+    fbq('track', 'Purchase', {currency: giveFBPT.currency, value: giveFBPT.amount});
 }
