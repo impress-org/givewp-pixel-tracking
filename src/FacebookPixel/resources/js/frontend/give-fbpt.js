f2331be23254a73d8030212dd34adb2307ae76ab
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-10-21 10:22:08 -0400

    fix: use stronger typeof check

--- src/FacebookPixel/resources/js/frontend/give-fbpt.js
+++ src/FacebookPixel/resources/js/frontend/give-fbpt.js
@@ -1,7 +1,7 @@
 const { fbq } = window.parent ? window.parent : window;
 const { giveFBPT } = window;
 
-if (typeof fbq !== "undefined") { 
+if (typeof fbq === "function") { 
     fbq('track', 'Donate' );
     fbq('track', 'Purchase', {currency: giveFBPT.currency, value: giveFBPT.amount});
 }
