c53fe54da0eda739abbcb4ad76cc33b9d2b6bde5
Henry Holtgeerts <henryholtgeerts@gmail.com>
2020-10-26 15:31:36 -0400

    feature: trigger CompleteRegistration FB pixel event

--- src/FacebookPixel/resources/js/frontend/give-fbpt.js
+++ src/FacebookPixel/resources/js/frontend/give-fbpt.js
@@ -4,4 +4,5 @@ const { giveFBPT } = window;
 if (typeof fbq === "function") { 
     fbq('track', 'Donate' );
     fbq('track', 'Purchase', {currency: giveFBPT.currency, value: giveFBPT.amount});
+    giveFBPT.isNewRegistration && fbq('track', 'CompleteRegistration');
 }
